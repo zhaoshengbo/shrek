26c49de5bb8d5f04559320ef7d31b6606a429cc8
zhaoshb <zhaoshengbo@126.com>
2014-08-05 22:46:27 +0800

    代理器提交.

---
+++ project/src/com/uidesigner/delegator/AddTagEventDelegator.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package com.uidesigner.delegator;
+
+import com.uidesigner.itf.IEventDelegator;
+
+/**
+ * Shrek.
+ *
+ * 新增标签事件代理类.
+ *
+ * @author zhaoshb
+ * @since 1.0
+ * @time 2014年8月5日 上午9:14:32
+ */
+public class AddTagEventDelegator implements IEventDelegator {
+
+	@Override
+	public void delegate() {
+	}
+}
